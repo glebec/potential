9abbf39a6304c77749b83160bbefbb4b004b106f
Gabriel Lebec <glebec@gmail.com>
2015-07-05 22:53:08 -0400

    passing all specs

--- pledge.js
+++ pledge.js
@@ -1,108 +1,140 @@
-// Private
+'use strict';
+//===== Private =====
 
-var HandlerGroup = function HandlerGroup (successCb, failureCb) {
-  this.successCb = (typeof successCb === 'function') ? successCb : null;
-  this.failureCb = (typeof failureCb === 'function') ? failureCb : null;
-  this.forwarder = new Deferral();
-};
+// helpers for more readable code
+function invoke (fns) { fns.forEach(function (fn) { fn(); }); }
+function isFunction (maybe) { return typeof maybe === 'function'; }
+function isObjectOrFunction (maybe) {
+  return maybe && (typeof maybe).match(/function|object/);
+}
 
-var $Promise = function $Promise () {
+// ancillary promise handler group constructor
+function ChainLink (onFulfilled, onRejected) {
+  this.onFulfilled = isFunction(onFulfilled) ? onFulfilled : null;
+  this.onRejected = isFunction(onRejected) ? onRejected : null;
+  this.downstream = new Deferral();
+}
+
+// making sure only platform code is on the stack before running handlers
+function Schedule () {
+  var fns = [];
+  this.add = fns.push.bind(fns);
+  this.run = function() { setImmediate(function(){ invoke(fns); }); };
+}
+
+//----- Promise class -----
+
+function $Promise () {
   this.state = 'pending';
-  this.handlers = [];
-  this.schedule = function schedule (fn) {
-    schedule.fns = schedule.fns || [];
-    if (fn) schedule.fns.push(fn);
-    else setImmediate(function(){
-      var newIndex = schedule.fns.length;
-      schedule.fns.forEach(function(fn){ fn(); });
-      schedule.fns = schedule.fns.slice(newIndex);
-    });
-  };
-};
+  this.chains = [];
+}
 
-$Promise.prototype.then = function then (successCb, failureCb) {
-  var newGroup = new HandlerGroup(successCb, failureCb);
-  this.handlers.push(newGroup);
+$Promise.prototype.then = function then (onFulfilled, onRejected) {
+  var newChain = new ChainLink(onFulfilled, onRejected);
+  this.chains.push(newChain);
   this.tryHandlers();
-  return newGroup.forwarder.promise;
+  return newChain.downstream.promise;
 };
 
 $Promise.prototype.tryHandlers = function tryHandlers () {
-  var group, handler, bubble, output;
   if (this.state === 'pending') return;
-  while (this.handlers.length) {
-    group = this.handlers.shift();
-    handler = (this.state === 'resolved') ? group.successCb : group.failureCb;
-    if (!handler) {
-      bubble = (this.state === 'resolved') ? 'resolve' : 'reject';
-      group.forwarder[bubble](this.value);
-    } else {
-      this.schedule(executes(handler, this.value, group.forwarder));
-    }
+  var chain, handler, schedule = new Schedule();
+  while (this.chains.length) {
+    chain = this.chains.shift();
+    handler = this.state === 'resolved' ? chain.onFulfilled : chain.onRejected;
+    if (handler) schedule.add(executes(handler, this.value, chain.downstream));
+    else if (this.state === 'resolved') chain.downstream.resolve(this.value);
+    else if (this.state === 'rejected') chain.downstream.reject(this.value);
   }
-  this.schedule();
+  schedule.run();
 };
 
-function executes (handler, value, forwarder) {
+function executes (handler, value, downstream) {
   return function executing () {
     try {
-      output = handler(value);
-      if (output === forwarder.promise) {
-        var er = new TypeError('cannot forward the promise returned by .then');
-        forwarder.reject(er);
-      } else if (output instanceof $Promise) {
-        output.then(
-          function(value) { forwarder.resolve(value); },
-          function(error) { forwarder.reject(error); }
-        );
-      } else forwarder.resolve(output);
-    } catch (err) {
-      forwarder.reject(err);
-    }
+      var x = handler(value);
+      promiseResolutionProcedure(downstream, x);
+    } catch (err) { downstream.reject(err); }
   };
 }
 
-var Deferral = function Deferral () {
+function promiseResolutionProcedure (downstream, x) { // 2.3
+  var promise = downstream.promise,
+      resolve = downstream.resolve.bind(downstream),
+      reject = downstream.reject.bind(downstream);
+  if (promise === x) { // 2.3.1
+    reject( new TypeError('handlers must not return current chain') );
+  } else if ( x instanceof $Promise ) { // 2.3.2
+    x.then( promiseResolutionProcedure.bind(downstream), reject );
+  } else if ( isObjectOrFunction(x) ) { // 2.3.3
+    try {
+      var then = x.then; // 2.3.3.1
+      if ( isFunction(then) ) { // 2.3.3.3
+        var ran = 0; // 2.3.3.3.3
+        try {
+          then.call(
+            x,
+            function resolvePromise (y) {
+              if (!ran++) promiseResolutionProcedure(downstream, y); // 2.3.3.3.1
+            },
+            function rejectPromise (y) { if (!ran++) reject(y); } // 2.3.3.3.2
+          );
+        } catch (errCallingThen) { if (!ran) reject(errCallingThen); } // 2.3.3.3.4
+      } else resolve(x); // 2.3.3.4
+    } catch (errAccessingThen) { reject(errAccessingThen); } // 2.3.3.2
+  } else { // 2.3.4
+    resolve(x);
+  }
+}
+
+//----- Deferral class -----
+
+function Deferral () {
   this.promise = new $Promise();
   this.resolve = this.resolve.bind(this);
-  this.reject  = this.reject.bind(this);
+  this.reject = this.reject.bind(this);
+}
+
+Deferral.prototype.settle = function settle (state, value) {
+  if (this.promise.state === 'pending' && state.match(/resolved|rejected/) ) {
+    this.promise.state = state;
+    this.promise.value = value;
+    this.promise.tryHandlers();
+  }
+  return this.promise;
 };
 
 Deferral.prototype.resolve = function resolve (value) {
-  if (this.promise.state !== 'pending') return;
-  this.promise.state = 'resolved';
-  this.promise.value = value;
-  this.promise.tryHandlers();
-  return this.promise;
+  return this.settle('resolved', value);
 };
 
 Deferral.prototype.reject = function reject (reason) {
-  if (this.promise.state !== 'pending') return;
-  this.promise.state = 'rejected';
-  this.promise.value = reason;
-  this.promise.tryHandlers();
-  return this.promise;
+  return this.settle('rejected', reason);
 };
 
-// Public
+//===== Public =====
 
-// Library & ES6-style constructor
-var Pledge = function Pledge (handOff) {
-  var deferral = this.defer();
-  handOff( deferral.resolve, deferral.reject );
+// Library namespace & ES6-style constructor wrapper
+function Pledge (executor) {
+  if (!isFunction(executor)) throw new TypeError('Pledge takes a function');
+  var deferral = Pledge.defer();
+  executor( deferral.resolve.bind(deferral), deferral.reject.bind(deferral) );
   return deferral.promise;
-};
+}
 
-// Deferral-style factory
+// Deferral factory function
 Pledge.defer = function defer () {
   return new Deferral();
 };
 
-// Utility methods
+//----- Library methods -----
+
+// returns a new promise resolved with `value`
 Pledge.resolved = function resolved (value) {
   return new Deferral().resolve(value);
 };
 
+// returns a new promise rejected with `reason`
 Pledge.rejected = function rejected (reason) {
   return new Deferral().reject(reason);
 };
